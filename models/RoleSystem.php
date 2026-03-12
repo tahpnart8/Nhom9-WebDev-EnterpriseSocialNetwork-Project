76fc4abf3195d8f00a2f733bf6278376ff2c3c0e
Nguyễn Lê Hải Long <whitenguyen902@gmail.com>
2026-03-12 08:55:47 +0700

    first commit của Long
    code 4 class: role, task, user, rolesystem

---
+++ models/RoleSystem.php
@@ -0,0 +1,24 @@
+<?php
+
+class RoleSystem {
+    private $roleName;
+    private $description;
+
+    public function __construct($roleName, $description) {
+        $this->roleName = $roleName;
+        $this->description = $description;
+    }
+
+    public function getRoleName() {
+        return $this->roleName;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+}
+?>
